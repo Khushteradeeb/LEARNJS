b5edf30ee83dc937b2462c180ed9f39bf01313b9
Khushter Adeeb <115522888+Khushteradeeb@users.noreply.github.com>
2026-03-02 20:17:20 +0000

    session completed

---
+++ script.js
@@ -0,0 +1,92 @@
+// console.log("Khushter Adeeb")
+
+//var score = 34;
+//var score = 56; // var gives prmission to redeclare  
+//console.log( score)
+
+//let points = 23; 
+//console .log(points)   
+   
+// Constant value = 10;
+
+//const value = 10;
+//console.log(value)
+//**************************************
+ // Data Types of Value
+ // 7 data Types 
+
+ // Numbers 1,2,22,33,76,3.4
+
+ // String "I am String , "34" , 'Also String'
+
+ // Boolean true/False
+
+ // Null Intentionally absense of value
+// let score = null;
+// score = 34;
+// console.log(score)
+
+// Undefined Variable declared but not initialised 
+
+// let points;
+// points = 13
+// console.log(points)
+
+// Object Complex Data Types
+
+// Symbol looks like Object
+
+
+/********************
+STRING
+*********************/
+
+// console.log("Hi Adeeb")
+let firstName = "Khushter"
+let lastName = "Adeeb"
+// console.log(firstName,lastName)
+// STRING CANCATINATION *************
+  
+// METHOD 1 Using + Operator 
+//  let fullName = firstName +" "+ lastName
+
+// Method 2 Using Template litrals
+//let fullName = `I want to become ${firstName} ${lastName}`
+//console.log(fullName)
+
+// Getting String Character *******
+// console.log(firstName[2])
+
+// STRING METHODS
+// console.log(firstName.toUpperCase())
+// console.log(lastName.indexOf("e"))
+
+// Common String Methods********
+//let hobbies = '    coding   reading running    ';
+//let result = hobbies.trim()
+// console.log(hobbies)
+//console.log(result)
+
+// indexOf
+//console.log(result.indexOf("coding"))
+
+// lastIndexOf
+//console.log(result.lastIndexOf("sleeping"))
+
+
+// INCLUDE METHOD 
+
+//console.log(result.includes("reading"))
+
+// let fullName = "WowProgrammer"
+
+//console.log(fullName.slice(0,8)) // Excluding last one 0-8
+
+//let result = fullName.slice(0,4) // last one 0-3
+
+// *** IMP NOTE Does not Mutate original Array
+
+// console.log("original String = ",fullName)
+// console.log("Extracted String =",result)
+
+ console.log("JUST Testing")
